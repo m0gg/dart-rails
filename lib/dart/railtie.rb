07f7fcba13d7ecc6a05bd943231df7cc309671a0
Marcel Sackermann <marcel@iras-revier.de>
2014-08-29 20:52:21 +0200

    Update to v0.1.0
     * total rework of transcompilation-feature
    
     * Note that `.dart2js` templates are no longer needed. Sprockets
     DirectiveProcessor for Javascript ist now capable of including
     dart2js-compiled scripts via a `//= dart dart_app` directive. See
     updated sample application on github.
    
     * Errors during dart2js compilation will now be handled by Sprockets
     and thus the exception message will be thrown in the created js file.
     For now there will be also an output on the console.
    
     * As for newest SprocketsRails there is no longer the need of manually
     adding `dart.js` to the precompilation list.
    
     * Compiled scripts will reside in the `#{Rails.root}/tmp/cache/` directory.

--- lib/dart/railtie.rb
+++ lib/dart/railtie.rb
@@ -2,8 +2,7 @@ require 'rails'
 require 'rails/railtie'
 require 'dart_js'
 require 'sprockets'
-require 'dart/tilt/dart_js_template'
-require 'dart/tilt/dart2js_template'
+require 'dart/tilt/sprockets_directive_dart2js'
 require 'dart/rails/engine'
 require 'dart/rails/template_handler'
 require 'dart/rails/version'
@@ -12,12 +11,24 @@ require 'dart/rails/generators/assets/generator'
 
 module Dart
   class Railtie < ::Rails::Railtie
+
+    initializer :assets do |config|
+      ::Rails.application.config.assets.precompile << 'dart.js'
+    end
+
     config.after_initialize do |app|
+      Dart::DART2JS_OUT_DIR = ::Rails.root.join('tmp', 'cache')
 
       # Register mime-types and Tilt-templates in assets environment
-      app.assets.register_mime_type 'application/dart2js', '.dart2js'
       app.assets.register_mime_type 'application/dart', '.dart'
-      app.assets.register_processor 'application/dart2js', Dart::Tilt::Dart2jsTemplate
+
+      # make dart2js results accessible for sprockets
+      app.assets.append_path(Dart::DART2JS_OUT_DIR)
+
+      # Mixin process_dart_directive in standard DirectiveProcessor for JS
+      Sprockets::DirectiveProcessor.instance_eval do
+        include Dart::SprocketsDirectiveDart2js
+      end
 
       # Mixin helper-module in ActionView
       ActionView::Base.instance_eval do
