760bfb2bef597c54fa375da54168ad94bf833141
Marcel Sackermann <marcel@iras-revier.de>
2014-10-01 15:27:24 +0200

    Recogizing changes to dart files
    Now all depending dart files are added to dependencies and the
    timestamp of the dart_app.js gets updated, allowing automatic
    recompiling on file-changes

--- lib/dart/sprockets/directive_processor_dart2js.rb
+++ lib/dart/sprockets/directive_processor_dart2js.rb
@@ -4,22 +4,53 @@ module Dart
   module DirectiveProcessorDart2js
     def process_dart_directive(path)
       pathname = context.resolve([path, '.dart'].join)
+      pathname_js = context.resolve([path, '.js'].join)
 
       ensure_dart2js_out_dir_exists
 
       transcoder = DartJs.new(File.new(pathname), options.merge({ out_dir: Dart::DART2JS_OUT_DIR }))
       result = transcoder.compile
 
+      #update Timestamp so we can compare possible changes
+      FileUtils.touch pathname_js
+
       if result.respond_to?(:exception)
         puts "\n------------- dart2js compilation exception -------------\n#{result.message}\n#{result.result}\n------------- dart2js compilation exception -------------"
         raise result
       end
 
-      context.depend_on_asset(transcoder.out_file)
+      context.depend_on(pathname)
+      dart_get_dependencies(pathname).each do |dep|
+        context.depend_on(dep)
+      end
       included_pathnames << transcoder.out_file
     end
 
     private
+
+    DART_EGREP = /^(import|part) ['"]((dart:){0}|(package:))[0-9a-zA-Z_\/\.\-]+\.dart['"];/
+    DART_EGREP_PATH = /['"](package:)?(.*)['"]/
+
+    def dart_get_dependencies(pathname)
+      deps = []
+      if File.exists?(pathname)
+        f = File.open(pathname)
+        f.each do |l|
+          if DART_EGREP =~ l
+            m = l.match(DART_EGREP_PATH)
+            dep_path = (m[1] ? "packages/#{m[2]}" : m[2])
+            deps << context.resolve(dep_path)
+          end
+        end
+        f.close
+        rdeps = []
+        deps.each do |dep|
+          rdeps += dart_get_dependencies(dep)
+        end
+        return (deps + rdeps).uniq
+      end
+      return nil
+    end
 
     def ensure_dart2js_out_dir_exists
       FileUtils.mkdir_p Dart::DART2JS_OUT_DIR unless File.directory?(Dart::DART2JS_OUT_DIR)
