a8eee65d1f15ef76d0e326bbfb22895dd9c306e0
Marcel Sackermann <marcel@iras-revier.de>
2014-10-18 05:25:36 +0200

    Fix recursion and path for import/part

--- lib/dart/sprockets/directive_processor_dart2js.rb
+++ lib/dart/sprockets/directive_processor_dart2js.rb
@@ -5,21 +5,22 @@ module Dart
     def process_dart_directive(path)
       dart_path = context.resolve([path, '.dart'].join)
 
-      dart2js_compiler = ::Dart2Js.new(File.new(dart_path), options.merge({ out_dir: dart2js_out_dir }))
-      result = dart2js_compiler.compile
+      context.depend_on(dart_path)
+      dart_get_dependencies(dart_path).each do |dep|
+        context.depend_on(dep)
+      end
 
       #update Timestamp so we can compare possible changes
       FileUtils.touch @pathname
+
+      dart2js_compiler = ::Dart2Js.new(File.new(dart_path), options.merge({ out_dir: dart2js_out_dir }))
+      result = dart2js_compiler.compile
 
       if result.respond_to?(:exception)
         puts "\n------------- dart2js compilation exception -------------\n#{result.message}\n#{result.result}\n------------- dart2js compilation exception -------------"
         raise result
       end
 
-      context.depend_on(dart_path)
-      dart_get_dependencies(dart_path).each do |dep|
-        context.depend_on(dep)
-      end
       included_pathnames << dart2js_compiler.out_file
     end
 
@@ -28,13 +29,17 @@ module Dart
     DART_EGREP = /^(import|part) ['"]((dart:){0}|(package:))[0-9a-zA-Z_\/\.\-]+\.dart['"];/
     DART_EGREP_PATH = /['"](package:)?(.*)['"]/
 
-    def dart_get_dependencies(pathname)
-      deps = dart_find_imports(pathname)
+    def dart_get_dependencies(pathname, exceptions=[])
+      deps = []
       #recursive search
-      deps += deps.map do |dep|
-        dart_get_dependencies(dep)
+      imports = dart_find_imports(pathname)
+      imports.each do |dep|
+        deps << dep unless deps.include?(dep)
       end
-      deps.uniq.flatten
+      (deps-exceptions).each do |path|
+        dart_get_dependencies(path, deps)
+      end
+      deps
     end
 
     def dart_find_imports(pathname)
@@ -44,8 +49,7 @@ module Dart
         next unless DART_EGREP =~ l
         m = l.match(DART_EGREP_PATH)
         #package or part?
-        dep_path = (m[1] ? "packages/#{m[2]}" : m[2])
-        deps << context.resolve(dep_path)
+        deps << (m[1] ? context.resolve("packages/#{m[2]}") : "#{pathname.to_s.slice /^.*\//}#{m[2]}")
       end
       deps
     end
