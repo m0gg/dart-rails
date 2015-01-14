4c47cd896cf88a4a58e1ef2064b1d71f43507df7
Marcel Sackermann <marcel@iras-revier.de>
2015-01-14 19:33:38 +0100

    Fix issue with rake assets:precompile support
    Asset procompilation would not initialize assets and thus the whitelist
    for non-stupid-digest-assets would keep empty, this results in every
    asset beeing non-digested.

--- lib/dart/railtie.rb
+++ lib/dart/railtie.rb
@@ -11,6 +11,23 @@ require 'dart/rails/template_handler'
 module Dart #:nodoc:
   class Railtie < ::Rails::Railtie
 
+    # [Optionally via: https://github.com/m0gg/non-stupid-digest-assets]
+    # do not digest .dart files
+    # digest during compilation breaks darts 'import' functionality
+    # currently sprockets-rails does not allow mixed procompilation of digest and non-digest assets
+    # see https://github.com/rails/sprockets-rails/issues/49
+    # workaround is a 51-liner gem 'non-stupid-digest-assets'
+    # https://github.com/alexspeller/non-stupid-digest-assets
+    #
+    begin
+      require 'non-stupid-digest-assets'
+      if defined? NonStupidDigestAssets
+        NonStupidDigestAssets.whitelist += [ /.*\.dart/, 'dart_app.js', 'dart_app' ]
+      end
+    rescue Exception => e
+      ::Rails.logger.info 'No non-stupid-digest-assets support. You may face issues with native dart-support!'
+    end
+
     config.after_initialize do |app|
       # Mixin helper-module in ActionView
       ActionView::Base.instance_eval do
