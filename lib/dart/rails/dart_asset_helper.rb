54c48adcbd528dc217b6615aff8bec2e5ab5192a
h <giantcat99@gmail.com>
2015-12-18 21:25:12 +0900

    added auto_dart_include_tag

--- lib/dart/rails/dart_asset_helper.rb
+++ lib/dart/rails/dart_asset_helper.rb
@@ -27,7 +27,43 @@ module Dart #:nodoc:
       def dart_path(source, options = {})
         path_to_asset(source, { :extname => '.dart' }.merge!(options))
       end
-
+      
+      # returns a string containing a html script tag.
+      # The src value will vary depending on the detected user agent.
+      # "assets/dart_app.dart2js" if accessed from dartium.
+      # "assets/dart_app.js" if accessed from other browsers.
+      def auto_dart_include_tag(*args)
+        opts = args.extract_options!
+        tags = args.uniq.map do |fileName|
+          if request.env['HTTP_USER_AGENT'].include? 'Dart'
+            _to_dart_tag fileName, opts
+          else
+            _to_js_tag fileName, opts
+          end
+        end
+        return tags.join('\n').html_safe
+      end
+      # options are always empty,currently
+      def _to_dart_tag fileName, options
+        tag_attributes = {
+           'src':dart_path(fileName, options),
+           'type':'application/dart'
+        }
+        tag_attributes = tag_attributes.merge options
+        #generates a tag
+        content_tag(:script, '',tag_attributes)
+      end
+      
+      def _to_js_tag fileName, options
+        options = {extname:'.dart2js'}.merge! options
+        tag_attributes = {
+          'src':path_to_asset(fileName, options),
+           'type':'application/javascript'
+        }
+        tag_attributes = tag_attributes.merge options
+        #generating a tag
+        content_tag(:script, '',tag_attributes)
+      end
       alias_method :path_to_dart, :dart_path
     end
   end
